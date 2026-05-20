044302988bb7505e5b12fca21f51a6ea6ac435c4
Raphaelima7-bit <Raphaelima7@hotmail.com>
2026-05-19 21:33:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -40,11 +40,11 @@ nascimentoInput.addEventListener("input", function (e) {
     let valor = e.target.value.replace(/\D/g, "");
 
     if (valor.length > 2) {
-        valor = valor.replace(/^(\d{2})(\d)/, "$1/$2");
+        valor = valor.substring(0, 2) + "/" + valor.substring(2);
     }
 
     if (valor.length > 5) {
-        valor = valor.replace(/^(\d{2})\/(\d{2})(\d)/, "$1/$2/$3");
+        valor = valor.substring(0, 5) + "/" + valor.substring(5, 9);
     }
 
     e.target.value = valor;
